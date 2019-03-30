bf42e436ab23340b576e434ffb9a66986a7c53cb
kalitawojciech <wojtek1311997@gmail.com>
2019-03-30 12:33:13 +0100

    created architecure

--- firstApi.Api/Program.cs
+++ firstApi.Api/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace firstApi
+namespace firstApi.Api
 {
     public class Program
     {
--- firstApi.Api/Startup.cs
+++ firstApi.Api/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace firstApi
+namespace firstApi.Api
 {
     public class Startup
     {
@@ -15,7 +15,6 @@ namespace firstApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,7 +24,7 @@ namespace firstApi
             {
                 app.UseDeveloperExceptionPage();
             }
-            app.UseMvc();
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
