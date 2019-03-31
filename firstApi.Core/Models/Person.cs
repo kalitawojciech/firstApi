ea03bf62ef6eeb0ed0144ae4b92a781b8bb9c4f8
kalitawojciech <wojtek1311997@gmail.com>
2019-03-31 15:03:54 +0200

    added Person model

---
+++ firstApi.Core/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace firstApi.Core.Models
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Description { get; set; }
+    }
+}
