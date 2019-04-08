f99b3b81843959fda8e9c1c7cb6eb3d746804789
kalitawojciech <wojtek1311997@gmail.com>
2019-04-08 22:55:22 +0200

    added Entities

---
+++ firstApi.Core/Entities/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace firstApi.Core.Entities
+{
+    class Person
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<Quote> Quotes { get; set; } = new List<Quote>();
+    }
+}
---
+++ firstApi.Core/Entities/Quote.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace firstApi.Core.Entities
+{
+    class Quote
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Description { get; set; }
+        [ForeignKey("PersonId")]
+        public Person Person { get; set; }
+        public int PersonId { get; set; }
+    }
+}
